c7650ceab29ee66d9e36fa0369e7e4b67f73df1b
rG <rg@pop-os.localdomain>
2020-11-22 20:57:46 +0530

    Add script for data-preparation.

---
+++ RScript/Start.R
@@ -0,0 +1,34 @@
+# ---------------Load Libraries---------------
+library(ggplot2)
+
+# ---------------Reading Data---------------
+data <- read.csv("Data/diabetic_data.csv")
+
+# ---------------Preparing Data---------------
+data[data == "?"] <- NA # Replace ? with NA
+str(data) # Show description of data
+
+navals <- is.na(data) # Creates a dataframe (having logical values) for missing data
+# Representing TRUE by 1 and FALSE by 0 
+navals[navals == TRUE] <- 1
+navals[navals == FALSE] <- 0
+
+# Taking column-wise sum for all variables
+nacount <- data.frame(colSums(navals)[colSums(navals) > 0])
+nacount
+
+# Adding variable names as a column
+rownames(nacount)
+nacount <- cbind.data.frame(nacount, rownames(nacount))
+colnames(nacount) <- c("NAcount", "Variables")
+nacount
+
+# ---------------Plotting Graphs---------------
+p <- ggplot(data = nacount, aes(x = Variables, y = NAcount)) +
+  geom_bar(stat = "identity", fill = "steelblue") +
+  geom_text(aes(label = NAcount), vjust = -0.3, size = 3.5) +
+  theme_minimal() +
+  coord_flip() +
+  labs(title = "Plot of Number of NA in a variable",
+  x = "Number of Missing Values", y = "Variable Name")
+p
