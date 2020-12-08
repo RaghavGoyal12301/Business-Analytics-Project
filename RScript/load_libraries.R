57f1cc598e4ff659b5f57f7f529491b0ab5bb448
RaghavGoyal12301 <12301raghavgoyal@gmail.com>
2020-12-09 02:20:23 +0530

    Add install functions for packages.

--- RScript/load_libraries.R
+++ RScript/load_libraries.R
@@ -1,7 +1,18 @@
+# ------------------------------Install Libraries------------------------------
+# install.packages("plyr")
+# install.packages("dplyr")
+# install.packages("stringr")
+# install.packages("randomForest")
+# install.packages("caret")
+# install.packages("e1071")
+
+
+
+
 # ------------------------------Load Libraries------------------------------
 library(plyr)
 library(dplyr)
 library(stringr)
 library(randomForest)
 library(caret)
-# library(ROSE)
+library(ROSE)
